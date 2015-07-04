857b97cefe450f76ffbebbe6c5e342b258768789
Grégory Chatel <chatel.gregory@gmail.com>
2015-07-04 14:51:48 +0200

    Adding commentaries.

--- draw_float_pil_process.py
+++ draw_float_pil_process.py
@@ -22,8 +22,8 @@ def compute_lines(x_center, y_center, point_number, multiplier):
     return res
 
 def draw_lines(img, x, y, radius, lines):
-    """This function uses gfxdraw to draw the initial circle in white and
-    the previously computed lines in red.
+    """This function uses ImageDraw.Draw to draw the initial circle in
+    white and the previously computed lines in red.
 
     """
     draw = ImageDraw.Draw(img)
@@ -32,10 +32,13 @@ def draw_lines(img, x, y, radius, lines):
         draw.line((p1, p2), fill = (255, 0, 0))
 
 def generate_image(args):
+    """This function creates the image, compute the lines given the
+    current parameters, draw the lines and save the image.
+
+    """
     filename, x_center, y_center, point_number, multiplier = args
-    print("Launching render with: filename=", filename, "x_center=", \
-          x_center, "y_center=", y_center, "point_number=", point_number, 
-          "multiplier=", multiplier)
+    print("filename ->", filename, "center =", (x_center, y_center) , "points ->",
+          point_number, "multiplier ->", multiplier)
     img = Image.new('RGB', (2 * x_center, 2 * y_center))
     lines = compute_lines(x_center, y_center, point_number, multiplier)
     draw_lines(img, x_center, y_center, radius, lines)
@@ -43,6 +46,11 @@ def generate_image(args):
 
 def iterate_range(steps, x_center, y_center, radius, param_sequence,
                   folder_name):
+    """This function creates the list of arguments taht will be used to
+    compute images. Then, it creates the processus pool and compute
+    the images using multiprocessing.Pool.map().
+
+    """
     args_list = []
     file_number = 1
     for i in range(len(param_sequence) - 1):
@@ -62,8 +70,12 @@ def iterate_range(steps, x_center, y_center, radius, param_sequence,
     print("Computations done")
     
 if __name__ == '__main__':
+    # the name of the folder in which the images will be saved.
     folder_name = sys.argv[1]
+    # Radius of initial circle.
     radius = int(sys.argv[2])
+    # Number of transition the algorithm will to through to get from a
+    # formula of the list to the next one.
     steps = int(sys.argv[3])
     param_sequence = [(100, 17), (300, 17), (300, 47), (100, 47)]
 
@@ -76,4 +88,3 @@ if __name__ == '__main__':
     iterate_range(steps, x_center, y_center, radius, param_sequence, folder_name)
 #    lines = compute_lines(x_center, y_center, point_number, multiplier)
 #    draw_lines(x_center, y_center, radius, lines, 'pil_render.bmp')
-
